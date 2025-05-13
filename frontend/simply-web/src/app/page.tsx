df988b05ae219a50d9f29ef37d3c2779332a388c
Kaleb Stephens <kaleb.stephens@fhnw.edu>
2025-05-13 11:57:08 -0600

    updated css and next js of home page

--- frontend/simply-web/src/app/page.tsx
+++ frontend/simply-web/src/app/page.tsx
@@ -7,10 +7,18 @@ export default function HomePage() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome<br/> To <br/>Simply Cards</h1>
-      <button onClick={() => router.push("/enter")} className={styles.button}>
-        Enter
-      </button>
+      <div className={styles.titleImageRow}>
+        <h1 className={styles.title}>Welcome<br/> To<br/> Simply Cards</h1>
+        <div className={styles.imageContainer}>
+          <img src="/Cards.jpeg" alt="Cards" className={styles.cardsImage} />
+        </div>
+      </div>
+
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={() => window.location.href = "/enter"}>
+          Enter
+        </button>
+      </div>
     </div>
   );
 }
